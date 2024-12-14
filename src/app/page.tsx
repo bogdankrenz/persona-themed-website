cccccbe5372d0c2659774d5983760815767cc3a9
bogdankrenz <bogdan@kre.nz>
2024-12-14 11:43:07 +0100

    feat/hero: Intro styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
         {/* Main content */}
         <div className="relative text-center font-mono text-m">
           <Navigation />
-          <div className="relative p-10 max-w-sm sm:max-w-lg mx-auto">
-            <h2 className="text-5xl  text-white">
-              Hi 👋, I’m Bogdan, a software engineer from Cologne.
+          <div className="relative p-10 max-w-sm sm:max-w-xl ml-auto md:mr-20">
+            <h2 className="text-5xl text-white mix-blend-normal">
+              <span className="text-white mix-blend-difference">Hi</span> 👋 I’m
+              Bogdan, a software engineer from{" "}
+              <span className="text-white mix-blend-overlay">Cologne</span>
             </h2>
           </div>
         </div>
